73160ffa17093b3409afc6bc92d9ead99cca5465
anuradham002 <144935088+anuradham002@users.noreply.github.com>
2023-11-14 21:46:34 +0530

    Create S2.cpp

---
+++ S2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+        unordered_map<char, char> cunt1;
+        unordered_map<char, char> cunt2;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (cunt1[s[i]] == 0){ 
+                cunt1[s[i]] = t[i];
+            }
+
+            else if (cunt1[s[i]] != t[i]) 
+            return false;
+
+            if (cunt2[t[i]] == 0) {
+                cunt2[t[i]] = s[i];
+            }
+            else if (cunt2[t[i]] != s[i]) 
+            return false;
+        }
+
+        return true;
+    }
+    int main()
+    {
+        string s,t;
+        cin>>s;
+        cin>>t;
+        cout<<isIsomorphic(s,t);
+    }
